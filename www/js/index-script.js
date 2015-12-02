f343351ccc6c84740b1614b96b43729328eca457
Gabriel <tr4zhm4il@gmail.com>
2015-12-02 23:07:17 +0100

    Updated with new content

--- www/js/index-script.js
+++ www/js/index-script.js
@@ -1,35 +1,64 @@
-var randomScalingFactor = function(){ return Math.round(Math.random()*100)};
-var options = {responsive: true, scaleGridLineColor : "rgba(255,255,255,.05)", scaleLineColor: "rgba(255,255,255,.05)",     legendTemplate : "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<datasets.length; i++){%><li><span style=\"background-color:<%=datasets[i].strokeColor%>\"></span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>"
+var randomScalingFactor = function() {
+    return Math.round(Math.random() * 100)
 };
+var options = {
+    responsive: true,
+    scaleGridLineColor: "rgba(255,255,255,.05)",
+    scaleLineColor: "rgba(255,255,255,.05)",
+    legendTemplate: "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<datasets.length; i++){%><li><span style=\"background-color:<%=datasets[i].strokeColor%>\"></span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>",
+    legendTemplate : "<ul class=\"legend <%=name.toLowerCase()%>-legend\"><% for (var i=0; i<datasets.length; i++){%><li><span class=\"legendColor\" style=\"background-color:<%=datasets[i].strokeColor%>\"></span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>",
+    multiTooltipTemplate: "<%= value %> KWh"
+};
+
 var lineChartData;
-
-
-window.onload = function(){
+window.onload = function() {
     lineChartData = {
-        labels : ["Mon","Tue","Wed","Thur","Fri","Sat","Sun"],
-        datasets : [
-            {
-                label: "My First dataset",
-                fillColor : "rgba(1,183,125,0.2)",
-                strokeColor : "rgba(1,183,125,1)",
-                pointColor : "rgba(1,183,125,1)",
-                pointStrokeColor : "#fff",
-                pointHighlightFill : "#fff",
-                pointHighlightStroke : "rgba(220,220,220,1)",
-                data : [randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor()]
-            },
-            {
-                label: "My First dataset",
-                fillColor : "rgba(1,125,183,0.2)",
-                strokeColor : "rgba(1,125,183,1)",
-                pointColor : "rgba(1,125,183,1)",
-                pointStrokeColor : "#fff",
-                pointHighlightFill : "#fff",
-                pointHighlightStroke : "rgba(151,187,205,1)",
-                data : [randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor()]
-            }
-        ]
+        labels: ["Mon", "Tue", "Wed", "Thur", "Fri", "Sat", "Sun"],
+        datasets: [{
+            label: "Energy cost",
+            fillColor: "rgba(1,183,125,0.2)",
+            strokeColor: "rgba(1,183,125,1)",
+            pointColor: "rgba(1,183,125,1)",
+            pointStrokeColor: "#fff",
+            pointHighlightFill: "#fff",
+            pointHighlightStroke: "rgba(220,220,220,1)",
+            data: [32, 26, 24, 28, 40, 36, 45]
+        }]
+    }
+    ctx = document.getElementById("energraph").getContext("2d");
+    window.myLine = new Chart(ctx).Line(lineChartData, options);
+    lineChartData = {
+        labels: ["Mon", "Tue", "Wed", "Thur", "Fri", "Sat", "Sun"],
+        datasets: [{
+            label: "Your Usage",
+            fillColor: "rgba(1,183,125,0.2)",
+            strokeColor: "rgba(1,183,125,1)",
+            pointColor: "rgba(1,183,125,1)",
+            pointStrokeColor: "#fff",
+            pointHighlightFill: "#fff",
+            pointHighlightStroke: "rgba(220,220,220,1)",
+            data: [randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor()]
+        }, {
+            label: "National Average",
+            fillColor: "rgba(1,125,183,0.2)",
+            strokeColor: "rgba(1,125,183,1)",
+            pointColor: "rgba(1,125,183,1)",
+            pointStrokeColor: "#fff",
+            pointHighlightFill: "#fff",
+            pointHighlightStroke: "rgba(151,187,205,1)",
+            data: [randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor()]
+        }, {
+            label: "Sustainable Level",
+            fillColor: "rgba(200,200,200,0.2)",
+            strokeColor: "rgba(200,200,200,1)",
+            pointColor: "rgba(200,200,200,1)",
+            pointStrokeColor: "#fff",
+            pointHighlightFill: "#fff",
+            pointHighlightStroke: "rgba(151,187,205,1)",
+            data: [17, 15, 26, 24, 14, 16, 14]
+        }]
     }
     ctx = document.getElementById("linegraph").getContext("2d");
-    window.myLine = new Chart(ctx).Line(lineChartData, options);
+    chart1 = new Chart(ctx).Line(lineChartData, options);
+    $('#legend1').html(chart1.generateLegend());
 }
